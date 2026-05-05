4d12a735a25fe89f7f6ee7857e4956d986d547d0
Simon J McLaurin <simon.mclaurin@outlook.com>
2026-05-05 09:36:25 -0400

    feat: Add Anticheat settings tab to UI

---
+++ src/UI/Windows/Tabs/AnticheatTab.cs
@@ -0,0 +1,32 @@
+﻿using MalumMenu.anticheat;
+using UnityEngine;
+
+namespace MalumMenu
+{
+    internal class AnticheatTab : ITab
+    {
+        public string name => "Anticheat";
+        public void Draw()
+        {
+            GUILayout.BeginVertical(GUILayout.Width(MenuUI.windowWidth * 0.425f));
+            Anticheat.Enabled = GUILayout.Toggle(Anticheat.Enabled, "Enable HyperMenu Anticheat");
+            GUILayout.Space(5);
+            GUILayout.Label("RPCs that should be checked by the anticheat:");
+            foreach (var (rpcCall, handler) in Anticheat.RpcHandlers)
+            {
+                handler.Enabled = GUILayout.Toggle(handler.Enabled, $"{rpcCall}");
+            }
+            GUILayout.Space(5);
+            GUILayout.Label("When a cheater is detected:");
+            Anticheat.sendNotification = GUILayout.Toggle(Anticheat.sendNotification, "Send notification");
+            Anticheat.discardRpc = GUILayout.Toggle(Anticheat.discardRpc, "Discard RPC");
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label($"Punish the player with: {Anticheat.punishment}");
+            Anticheat.punishment = (Anticheat.Punishments)GUILayout.HorizontalSlider((float)Anticheat.punishment, 0, 3);
+            GUILayout.EndHorizontal();
+
+
+        }
+    }
+}
